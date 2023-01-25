b415f770b2e436ee2dd881c8cdc8a2e73fe3b895
rizzok <suncode13@gmail.com>
2023-01-25 19:22:10 +0100

    Training

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import TextField from './TextField'
+
 function App() {
   return (
     <div>
       <h1>Vite React TS</h1>
+      <TextField text="hello" person={{ firstName: 'John', lastName: 'Doe' }} />
     </div>
   )
 }
