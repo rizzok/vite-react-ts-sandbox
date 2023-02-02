2331bc6082a644eb62612dec75121e665da1f62a
rizzok <suncode13@gmail.com>
2023-02-02 21:55:48 +0100

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Counter from './components/Counter/Counter'
+import Header from './components/Header/Header'
 
 const App = () => {
   return (
     <>
+      <Header />
       <h1>TypeScript Sandbox</h1>
       <Counter />
     </>
--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -7,6 +7,9 @@ const StyledDiv = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  border: 1px solid white;
+  border-radius: 8px;
+  padding: 1rem;
 `
 
 const Counter: React.FC = () => {
